e42df814206f8824a4f1535b89c0dfec23834491
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-11-17 20:33:11 -0500

    code shoul dbe all there

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 TOKEN=''
+
+CRYPTO_RPC_PORT='51472'
+CRYPTO_RPC_USER='user'
+CRYPTO_RPC_PW='password'
+CRYPTO_RPC_IP='127.0.0.1'
+CRYPTO_TICKER="DOGEC"
+
+FAUCET_MIN_WITHDRAW = 0.1 #do we really need and upper withdraw, can implement if needed?
+FAUCET_REWARD=0.05
+FAUCET_TIME_DELAY=5 #in seconds
